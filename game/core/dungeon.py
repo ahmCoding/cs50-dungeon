5b0f93592bc290a6ebfbc401ab3ee9d8182a3beb
ahmCoding <amirhosseinm7@gmail.com>
2026-06-26 12:49:37 +0200

    Dungeon-class feature/multi-level

--- game/core/dungeon.py
+++ game/core/dungeon.py
@@ -7,18 +7,18 @@ class Dungeon:
     """
 
     def __init__(self, maps: list[Map]):
-        self.maps = maps
+        self._maps = maps
         self._idx_current_map = 0
 
     def get_current_map(self) -> Map:
-        return self.maps[self._idx_current_map]
+        return self._maps[self._idx_current_map]
 
-    def next_map(self) -> Map:
-        self._idx_current_map += 1
-        return self.get_current_map()
+    def next_map(self) -> None:
+        if not self.is_last_map():
+            self._idx_current_map += 1
 
     def is_last_map(self) -> bool:
         """
         :return: True if the current map is the last map
         """
-        return self._idx_current_map == len(self.maps) - 1
+        return self._idx_current_map == len(self._maps) - 1
