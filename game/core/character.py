dbeabd6f9e39b958bd69b333aad1b67e1b295853
ahmCoding <amirhosseinm7@gmail.com>
2026-06-29 22:19:36 +0200

    Refactor of the player class feature/first-enemy

---
+++ game/core/character.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class Character:
+    """
+    this class represents a character in the game
+    """
+
+    class Direction(Enum):
+        """
+        The Enum representing the direction of the character.
+        the first element of the tuple represents the x value for horizontal movement
+        the second element represents the y value for vertical movement
+        """
+
+        UP = (0, -1)
+        DOWN = (0, 1)
+        LEFT = (-1, 0)
+        RIGHT = (1, 0)
+
+    def __init__(self, x: int = 0, y: int = 0):
+        """
+        :param x: width
+        :param y: height
+        """
+        self.x = x
+        self.y = y
+
+    def move(self, direction: Direction):
+        dx, dy = direction.value
+        self.x += dx
+        self.y += dy
+
+    def set_position(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def get_position(self) -> tuple[int, int]:
+        """
+        :return: (x, y)
+        """
+        return self.x, self.y
--- game/core/player.py
+++ game/core/player.py
@@ -1,46 +1,11 @@
-from enum import Enum
+from game.core.character import Character
 
 
-class Player:
+class Player(Character):
     """
     this class represents a player in the game
     """
 
-    class Direction(Enum):
-        """
-        The Enum representing the direction of the player.
-        the first element of the tuple represents the x value for horizontal movement
-        the second element represents the y value for vertical movement
-        """
-
-        UP = (0, -1)
-        DOWN = (0, 1)
-        LEFT = (-1, 0)
-        RIGHT = (1, 0)
-
-    def __init__(self, x: int = 0, y: int = 0):
-        """
-        :param x: width
-        :param y: height
-        """
-        self.x = x
-        self.y = y
-
-    def move(self, direction: Direction):
-        dx, dy = direction.value
-        self.x += dx
-        self.y += dy
-
-    def next_position(self, direction: Direction) -> tuple[int, int]:
+    def next_position(self, direction: Character.Direction) -> tuple[int, int]:
         dx, dy = direction.value
         return self.x + dx, self.y + dy
-
-    def set_position(self, x: int, y: int) -> None:
-        self.x = x
-        self.y = y
-
-    def get_position(self) -> tuple[int, int]:
-        """
-        :return: (x, y)
-        """
-        return self.x, self.y
